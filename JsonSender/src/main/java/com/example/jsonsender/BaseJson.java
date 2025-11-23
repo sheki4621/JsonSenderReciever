4d158865d2a6b09bf54942b24abfa4b2b497dc9e
yoshiki <6.5.two.8@gmail.com>
2025-11-23 15:24:51 +0900

    共通部分をBaseJsonにくくりだし

---
+++ JsonSender/src/main/java/com/example/jsonsender/BaseJson.java
@@ -0,0 +1,62 @@
+package com.example.jsonsender;
+
+import com.example.jsonsender.notice.NoticeType;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+public abstract class BaseJson {
+    @JsonProperty("Id")
+    private UUID id;
+
+    @JsonProperty("NoticeType")
+    private NoticeType noticeType;
+
+    @JsonProperty("timestamp")
+    private ZonedDateTime timestamp;
+
+    @JsonProperty("Version")
+    private String version;
+
+    public BaseJson() {
+    }
+
+    public BaseJson(UUID id, NoticeType noticeType, ZonedDateTime timestamp, String version) {
+        this.id = id;
+        this.noticeType = noticeType;
+        this.timestamp = timestamp;
+        this.version = version;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public NoticeType getNoticeType() {
+        return noticeType;
+    }
+
+    public void setNoticeType(NoticeType noticeType) {
+        this.noticeType = noticeType;
+    }
+
+    public ZonedDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(ZonedDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
--- JsonSender/src/main/java/com/example/jsonsender/metrics/MetricsJson.java
+++ JsonSender/src/main/java/com/example/jsonsender/metrics/MetricsJson.java
@@ -5,63 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
-public class MetricsJson {
-    @JsonProperty("Id")
-    private UUID id;
-
-    @JsonProperty("NoticeType")
-    private NoticeType noticeType;
-
-    @JsonProperty("timestamp")
-    private ZonedDateTime timestamp;
-
-    @JsonProperty("Version")
-    private String version;
-
+public class MetricsJson extends com.example.jsonsender.BaseJson {
     @JsonProperty("Metrics")
     private Metrics metrics;
 
     public MetricsJson() {
+        super();
     }
 
     public MetricsJson(UUID id, NoticeType noticeType, ZonedDateTime timestamp, String version, Metrics metrics) {
-        this.id = id;
-        this.noticeType = noticeType;
-        this.timestamp = timestamp;
-        this.version = version;
+        super(id, noticeType, timestamp, version);
         this.metrics = metrics;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public NoticeType getNoticeType() {
-        return noticeType;
-    }
-
-    public void setNoticeType(NoticeType noticeType) {
-        this.noticeType = noticeType;
-    }
-
-    public ZonedDateTime getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(ZonedDateTime timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
     }
 
     public Metrics getMetrics() {
